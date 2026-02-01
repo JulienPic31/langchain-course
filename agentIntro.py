fe2a3f9fca676ccc3fe44bdfff114cfb11718720
JulienPic31 <pichejulien31@gmail.com>
2026-02-01 13:33:42 -0500

    Example of structuring the agent's response using a custom response schematic

--- agentIntro.py
+++ agentIntro.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 import tavily
 
@@ -21,11 +24,26 @@ from langchain_tavily import TavilySearch
 #     print(f"Searching for {query}")
 #     return tavily.search(query = query)
 
+# Example of defining a custom response schematic of our agent
+# The Source class defines the base structure that a source should have
+# The AgentResponse class then defines how the agent provides the answer to the prompt, in this case it provides the answer and a list of the sources it used to generate that answer
+class Source (BaseModel):
+    """Schematic for a source used by the agent"""
+
+    url: str = Field (description = "The URL of the source")
+
+class AgentResponse (BaseModel):
+    """Schematic for agent response with answer and sources"""
+
+    answer: str = Field (description = "The agent's answer to the query")
+    sources: List[Source] = Field (default_factory = list, description = "The list of sources used to generate the answer")
+
+
 
 llm = ChatOllama(model = "gpt-oss:20b", temperature = 0)
 # This tool definition uses the pre-defined TavilySearch tool from langchain_tavily (more accurate, tavily codes better than you do)
 tools = [TavilySearch()]
-agent = create_agent(model = llm, tools = tools)
+agent = create_agent(model = llm, tools = tools, response_format = AgentResponse)
 
 
 def main():
