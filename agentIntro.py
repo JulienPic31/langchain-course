0bcd1bb0ff661c1fe2b9b1c99b6f83c2c575faae
JulienPic31 <pichejulien31@gmail.com>
2026-02-01 13:11:42 -0500

    Showing the difference between a custom defined search tool and the pre-defined tavily_search tool.

--- agentIntro.py
+++ agentIntro.py
@@ -1,34 +1,36 @@
 from dotenv import load_dotenv
+import tavily
 
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over the internet
-
-    Args:
-        query: The query to search for
-    Returns:
-        The search results
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query = query)
+# Example of a custom defined tool (using the tavily package)
+# @tool
+# def search_tool(query: str) -> str:
+#     """
+#     Tool that searches over the internet 
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query = query)
 
 
 llm = ChatOllama(model = "gpt-oss:20b", temperature = 0)
-tools = [search]
+# This tool definition uses the pre-defined TavilySearch tool from langchain_tavily (more accurate, tavily codes better than you do)
+tools = [TavilySearch()]
 agent = create_agent(model = llm, tools = tools)
+
+
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content = "Search for 3 job postings for an ai engineer using langchain in Montreal on linkedin and list their details")})
+    result = agent.invoke({"messages": HumanMessage(content = "Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
     print(result)
 
 
