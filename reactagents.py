151cb03edbdb5f39d688a2f59e64abd3fe0d0c6d
JulienPic31 <pichejulien31@gmail.com>
2026-02-15 18:05:46 -0500

    react agents start section 26

---
+++ reactagents.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents import create_react_agent
+
+def main():
+    print("Hello from langchain-course!")
+
+
+
+if __name__ == "__main__":
+    main()
